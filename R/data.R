6fc9d90397eef3c92e7c5d7120ae63c97d0aa922
Yichen Jiang <yichejiang1@spartan-login1.hpc.unimelb.edu.au>
2025-12-25 17:41:26 +1100

    New Vignette ready

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Sample data used in the vignette
+#' Sample data used in the FlexiDeconv vignette
 #'
 #' @format It contains a list having:
 #'  1. Spatial Transcriptomics data generated from single-cell profiling of the
@@ -10,5 +10,18 @@
 #'
 #' @source \url{https://datadryad.org/dataset/doi:10.5061/dryad.8t8s248}
 #'
-#' @usage data(mouse_hypothalamus)
-"mouse_hypothalamus"
+#' @usage data(mouseHypothalamus)
+"mouseHypothalamus"
+
+
+
+#' Sample data used in the inference vignette
+#'
+#' @format It contains a deconvolution result from applying imperfect reference
+#' to the mouse hypothalamus data, in the standard FlexiDeconv output format
+#'
+#' @source \url{https://datadryad.org/dataset/doi:10.5061/dryad.8t8s248}
+#'
+#' @usage data(imperfectReferenceResult)
+"imperfectReferenceResult"
+
